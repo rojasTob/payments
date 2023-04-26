15ef3785d61b70bf009c6d251a5563cdd10cae34
Angie Rojas <nataliart4@gmail.com>
2023-04-26 16:03:18 -0500

    Add employees info and change the log of the information

--- app.js
+++ app.js
@@ -1,24 +1,37 @@
 import fs from 'node:fs/promises';
 import path from 'node:path';
-// const fs = require('fs/promises');
-// const path = require('path');
-// import dataParser from './parsers/dataParser';
+
+import { processPayment } from './controllers/calculator.js';
 
 
 const pathFile = path.join(process.cwd(), 'data', 'employees.txt');
-console.log('>> path >>', pathFile);
+console.info(`\x1b[33m The path for employee.txt is: ${ pathFile } \x1b[0m`);
 
 async function readFile() {
 
     try {
         const fileContents = await fs.readFile(pathFile, 'utf8');
-        console.log('content type: ', typeof fileContents);
-        console.log(fileContents);
-        return fileContents;
+        const fileLineByLine = fileContents.split(/\r?\n/);
+        return fileLineByLine;
     } catch (err) {
         console.error(`Error reading file: ${err}`);
     }
-    return '';
+    return [];
 }
-  
-readFile();
+
+function processFile(){
+    readFile().then((lines) => {
+        lines.forEach(line => {
+            const employee = processPayment(line);
+
+            if(employee.errors.length !== 0){
+                console.log(`Time entries for ${employee.name} have errors, please check them: ${employee.errors}`);
+            }else{
+                console.log(`The amount to pay ${employee.name} is: ${employee.totalAmount.toFixed(2)} USD.`);
+            }
+            console.log('----------------------------------------');
+        });
+    });
+}
+
+processFile();
