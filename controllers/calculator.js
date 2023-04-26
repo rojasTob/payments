fd096b1e43b35844d4730f9444253d42e1728ec5
Angie Rojas <nataliart4@gmail.com>
2023-04-26 16:00:59 -0500

    Changed the processPayment fn with the approach of separate timeframes

--- controllers/calculator.js
+++ controllers/calculator.js
@@ -1,16 +1,67 @@
 import { getEmployeeData, getTimeWorkedData } from '../parsers/dataParser.js';
-import { getPaymentValue } from '../utils/tablePayments.js';
 
-export function calculateAmount(lineFromFile){
+import { tableRanges, weekDays } from '../config/constants.js';
+
+const isWeekday = (day) => {
+    return weekDays.includes(day);
+}
+
+const separateTimeFrames = (startMinutes, endInMinutes) => {
+    const timeframes = [0 , 0 , 0];
+    if (endInMinutes > tableRanges[2].from){
+        if (startMinutes >= tableRanges[2].from) {
+            timeframes[2] = endInMinutes - startMinutes;
+            timeframes[1] = 0;
+            timeframes[0] = 0;
+        } else if (startMinutes >= tableRanges[1].from) {
+            timeframes[2] = endInMinutes - tableRanges[2].from;
+            timeframes[1] = tableRanges[1].to - startMinutes;
+            timeframes[0] = 0;
+        } else {
+            timeframes[2] = endInMinutes - tableRanges[2].from;
+            timeframes[1] = tableRanges[1].to - tableRanges[1].from;
+            timeframes[0] = tableRanges[0].to - startMinutes;
+        }
+    } else if (endInMinutes > tableRanges[1].from) {
+        if (startMinutes > tableRanges[1].from) {
+            timeframes[1] = endInMinutes - startMinutes;
+            timeframes[0] = 0;
+        } else {
+            timeframes[1] = endInMinutes - tableRanges[1].from;
+            timeframes[0] = tableRanges[0].to - startMinutes;
+        }
+    } else {
+        if (startMinutes > tableRanges[0].from) {
+            timeframes[0] = endInMinutes - startMinutes;
+        }
+        else {
+            timeframes[0] = endInMinutes - tableRanges[0].from;
+        }
+    }
+    return timeframes;
+}
+
+const calculateTimeFrameAmount = (timeFrame, tableRangeIndex, isWeekday) => {
+    return timeFrame/60 * tableRanges[tableRangeIndex][isWeekday ? 'weekDayValue' : 'weekendValue'];
+};
+
+export function processPayment(lineFromFile){
     const employeeData = getEmployeeData(lineFromFile);
     const workEntries = getTimeWorkedData(employeeData.timeWorked);
+    const errors = [];
 
     workEntries.forEach(entry => {
-        const valuePerHour = getPaymentValue(entry.day, entry.startInMinutes, entry.endInMinutes);
-        entry.valuePerHour = valuePerHour;
+        const timeframes = separateTimeFrames(entry.startInMinutes, entry.endInMinutes);
+        const isAWeekday = isWeekday(entry.day);
+
+        entry.total = calculateTimeFrameAmount(timeframes[0], 0, isAWeekday) + calculateTimeFrameAmount(timeframes[1], 1, isAWeekday) + calculateTimeFrameAmount(timeframes[2], 2, isAWeekday);
+
+        if(entry.error){
+            errors.push(entry.error);
+        }
     });
 
-    const totalAmount = workEntries.reduce((total, entry) => total + (entry.workedHours * entry.valuePerHour), 0);
+    const totalAmount = workEntries.reduce((total, entry) => total + entry.total, 0);
 
-    return `The amount to pay ${employeeData.name} is: ${totalAmount.toFixed(2)} USD`;
+    return { name: employeeData.name, totalAmount: totalAmount, errors: errors };
 }
--- controllers/calculator.test.js
+++ controllers/calculator.test.js
@@ -1,9 +1,29 @@
-import { calculateAmount } from './calculator.js';
+import { processPayment } from './calculator.js';
 
-test('given an employee input it returns the ampunt to pay', () => {
-    expect(calculateAmount('ASTRID=MO10:00-12:00,TH12:00-14:00,SU20:00-21:00')).toBe('The amount to pay ASTRID is: 85.00 USD');
+test('given a correct input it returns the name, totalAmount and no errors', () => {
+    const employee = processPayment('ASTRID=MO10:00-12:00,TH12:00-14:00,SU20:00-21:00');
+    expect(employee).toHaveProperty('name', 'ASTRID');
+    expect(employee).toHaveProperty('totalAmount', 85);
+    expect(employee).toHaveProperty('errors', []);
 });
 
-test('given an employee input it returns the ampunt to pay', () => {
-    expect(calculateAmount('RENE=MO10:00-12:00,TU10:00-12:00,TH01:00-03:00,SA14:00-18:00,SU20:00-21:00')).toBe('The amount to pay RENE is: 215.00 USD');
+test('given a correct input it returns the name, totalAmount and no errors', () => {
+    const employee = processPayment('ASTRID=TU09:01-20:00');
+    expect(employee).toHaveProperty('name', 'ASTRID');
+    expect(employee).toHaveProperty('totalAmount', 174.41666666666669);
+    expect(employee).toHaveProperty('errors', []);
+});
+
+test('given a correct input it returns the name, totalAmount and no errors', () => {
+    const employee = processPayment('RENE=MO10:00-12:00,TU10:00-12:00,TH01:00-03:00,SA14:00-18:00,SU20:00-21:00');
+    expect(employee).toHaveProperty('name', 'RENE');
+    expect(employee).toHaveProperty('totalAmount', 215);    
+    expect(employee).toHaveProperty('errors', []);
+});
+
+test('given an incorrect input it returns the name, totalAmount and errors', () => {
+    const employee = processPayment('DANIEL=TU12:30-10:00,TH09:20-17:20,FR19:00-23:00,QQ03:00-08:00');
+    expect(employee).toHaveProperty('name', 'DANIEL');
+    expect(employee).toHaveProperty('totalAmount', 199.08333333333331);
+    expect(employee).toHaveProperty('errors', ['TU12:30-10:00', 'QQ03:00-08:00']);
 });
