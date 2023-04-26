9154d682e2268a8ff035e4bb6e753e3e7a8893e4
Angie Rojas <nataliart4@gmail.com>
2023-04-26 15:56:38 -0500

    Add constants file which has the table payment information and days information

---
+++ config/constants.js
@@ -0,0 +1,9 @@
+export const tableRanges = [
+    { from: 1, to: 540, weekDayValue: 25, weekendValue: 30 },
+    { from: 541, to: 1080, weekDayValue: 15, weekendValue: 20 },
+    { from: 1081, to: 1440, weekDayValue: 20, weekendValue: 25 }
+];
+
+export const weekDays = ['MO', 'TU', 'WE', 'TH', 'FR'];
+
+export const daysAbbreviation = ['MO', 'TU', 'WE', 'TH', 'FR', 'SA', 'SU'];
